b710c41f05a44fc4c2aa584b10ffafda4eed635b
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2020-05-03 13:50:33 +0200

    Bash script error directives

--- scripts/app/build.dev.sh
+++ scripts/app/build.dev.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -eu # exit on error or undefined variable
+bash -c 'set -o pipefail' # return code of first cmd to fail in a pipeline
 
 echo "building app..."
 go build -ldflags "-X main.addr=:8118" -o /mnt/go-docker-debug/dev.out /mnt/go-docker-debug/main.go
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+set -eu # exit on error or undefined variable
+bash -c 'set -o pipefail' # return code of first cmd to fail in a pipeline
+
 NAME="go-docker-debug"
 if [[ $(tmux ls | grep ${NAME}) ]]; then
     echo "stop session..."
